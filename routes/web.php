79d4c4769d35cd48898102fde1cf7c3dd3711471
Redoc Tsuj <Redoc.Tsuj@gmail.com>
2017-02-10 19:17:45 +0200

    Designed top part of common layout

--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,10 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index');
+Route::get('/homepage', 'HomeController@index');
